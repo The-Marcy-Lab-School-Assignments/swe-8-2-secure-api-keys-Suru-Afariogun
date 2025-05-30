ede1108eeb989d403b15a492d00467786a239956
root <root@MARCY-LPT07.localdomain>
2025-05-30 12:46:15 -0400

    done but not deployed

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@
 
 const path = require('path');
 const express = require('express');
+// const { send } = require('process');
+require('dotenv').config();
 
 //////////////////////////
 // Constants
@@ -12,17 +14,27 @@ const express = require('express');
 const port = 8080;
 const pathToDistFolder = path.join(__dirname, '../frontend/dist');
 const app = express();
-
+const { API_Key } = process.env;
 //////////////////////////
 // Middleware/Controllers
 //////////////////////////
+const serveGifs = async (req, res) => {
+  try {
+    const url = `https://api.giphy.com/v1/gifs/trending?limit=3&rating=g&api_key=${API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    res.status(503).send(error);
+  }
+};
 
 const serveStatic = express.static(pathToDistFolder);
 
 app.use(serveStatic);
-
+app.get('/api/gifs', serveGifs);
 //////////////////////////
 // Listener
 //////////////////////////
 
-app.listen(port, () => console.log(`listening at http://localhost:${port}`)); 
+app.listen(port, () => console.log(`listening at http://localhost:${port}`));
